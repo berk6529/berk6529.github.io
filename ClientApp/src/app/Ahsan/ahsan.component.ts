fc7749ee7acc118e3f6da49e742bc12aa1a5e8bd
admin <brad.berkobien@navex.com>
2023-02-01 11:22:40 -0500

    add in css files to each component and register them

--- ClientApp/src/app/Ahsan/ahsan.component.ts
+++ ClientApp/src/app/Ahsan/ahsan.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ahsan',
   templateUrl: './ahsan.component.html',
+  styleUrls: ['./ahsan.component.css'],
+
 })
 export class AhsanComponent {
 }
--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -3,7 +3,8 @@ import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
+  styleUrls: ['./fetch-data.component.css'],
 })
 export class FetchDataComponent {
   nameList!: Array<names>;
