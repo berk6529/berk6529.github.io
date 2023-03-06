3fad6ef9da2a502c2e024cb8a598c4917b7a803f
Brad Berkobien <brad.berkobien@navex.com>
2023-03-06 18:00:13 -0500

    Add in missing piece

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -16,6 +16,9 @@ export class NavMenuComponent {
           $('.page__style').addClass('animate_content');
           $('.page__description').fadeOut(100).delay(2800).fadeIn();
         
+          setTimeout(function() {
+            $('.page__style').removeClass('animate_content');
+          }, 3200);
         
           //remove fadeIn class after 1500ms
           setTimeout(function() {
